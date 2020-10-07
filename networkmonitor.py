95039ac5b30f3b7cf1fdf382dbca082730f6e564
Mike <lush@xtra.co.nz>
2020-10-07 22:32:20 +1300

    Update networkmonitor.py

--- networkmonitor.py
+++ networkmonitor.py
@@ -109,7 +109,7 @@ class Mainframe(tk.Frame):
         #Exit and Connect Buttons
         img4=Image.open('/home/pi/DisneyStreaming/exit.png').resize((40,40))
         img4=ImageTk.PhotoImage(img4)
-        lbl1=tk.Button(self, image=img4, command=PopUpConfirmQuit(self), borderwidth=0, highlightthickness=0, bg='#0b0c1b')
+        lbl1=tk.Button(self, image=img4, command=lambda: PopUpConfirmQuit(self), borderwidth=0, highlightthickness=0, bg='#0b0c1b')
         lbl1.image=img4
         lbl1.grid(row=0, column=0, columnspan=3, rowspan=2)
 
