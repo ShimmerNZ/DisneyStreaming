1513c2a41c3dc431e5811a0c5998a635393189fe
Mike <lush@xtra.co.nz>
2020-10-08 22:08:46 +1300

    Update networkmonitor.py

--- networkmonitor.py
+++ networkmonitor.py
@@ -300,9 +300,8 @@ class Mainframe(tk.Frame):
             data=json.loads(requests.get(DATA_SOURCE).text)
             print(DATA_SOURCE)
             print(data)
-            print(data[DATA_LOCATION1])
-            print(data[DATA_LOCATION2])
-            print(data[DATA_LOCATION3])
+            print(data["subscriberCount"])
+
             #self.Subs=subs
         except RuntimeError as e:
             print("Some error occured, retrying! -", e)
