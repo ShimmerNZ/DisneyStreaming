85692c596555cacb624483c37b8981fae639bb1b
Mike <lush@xtra.co.nz>
2020-10-11 15:59:41 +1300

    fix logging

--- stream.sh
+++ stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/DisneyStreaming/
-/usr/bin/python3 "/home/pi/DisneyStreaming/networkmonitor.py"
+/usr/bin/python3 "/home/pi/DisneyStreaming/networkmonitor.py" > "/home/pi/logs/stream.log"
