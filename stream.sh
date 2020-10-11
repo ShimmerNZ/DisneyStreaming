a3ddba985ca56cbe82cb3bf16ab05c4401448ad2
Mike <lush@xtra.co.nz>
2020-10-11 16:11:21 +1300

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/DisneyStreaming/
-/usr/bin/python3 "/home/pi/DisneyStreaming/networkmonitor.py" > "/home/pi/logs/networkmonitor.log"
+/usr/bin/python3 "/home/pi/DisneyStreaming/networkmonitor.py" > "/home/pi/logs/stream.log"
