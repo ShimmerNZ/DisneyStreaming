a4bc6cb18489b81dae023297bdc18472475be8f2
Mike <lush@xtra.co.nz>
2020-10-11 14:07:54 +1300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 cd /home/pi/DisneyStreaming/
 git pull origin master
-sleep 3
 sudo cp /home/pi/DisneyStreaming/Update.txt /home/pi/Desktop/Update.desktop
 sudo cp /home/pi/DisneyStreaming/Stream.txt /home/pi/Desktop/Stream.desktop
 sudo cp /home/pi/DisneyStreaming/splash.png /usr/share/plymouth/themes/pix/splash.png
-sleep 3
+sleep 2
