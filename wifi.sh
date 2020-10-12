43ad79476c21a6087b118aa74e38121984885cab
Mike <lush@xtra.co.nz>
2020-10-12 20:54:28 +1300

    fixes

--- wifi.sh
+++ wifi.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/DisneyStreaming/
 sudo iptables -t mangle -A POSTROUTING -j TTL --ttl-set 65
-/usr/bin/python "/home/pi/DisneyStreaming/Wifi.py" > "/home/pi/logs/wifi.log"
+/usr/bin/python "/home/pi/DisneyStreaming/Wifi.py" >> "/home/pi/logs/wifi.log"
