c4ef94afd589fc8739ebb86dfe10b470f05519fb
Mike <lush@xtra.co.nz>
2020-10-17 18:58:58 +1300

    Update wifi.sh

--- wifi.sh
+++ wifi.sh
@@ -1,4 +1,4 @@
 cd /home#!/bin/bash
 cd /home/pi/DisneyStreaming/
 sudo iptables -t mangle -I POSTROUTING 1 -j TTL --ttl-set 65
-/usr/bin/python "/home/pi/DisneyStreaming/Wifi.py" >> "/home/pi/logs/wifi.log" 2&>1
+/usr/bin/python3 "/home/pi/DisneyStreaming/Wifi.py" >> "/home/pi/logs/wifi.log" 2&>1
