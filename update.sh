f4f01e4dc1923680ea33c6e171aaf75cc128ddb4
Mike <lush@xtra.co.nz>
2020-10-18 21:16:38 +1300

    move executables from git folder

--- update.sh
+++ update.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 cd /home/pi/DisneyStreaming/
-rm wifi.sh
-rm stream.sh
-rm update.sh
 git pull origin master
 sudo cp /home/pi/DisneyStreaming/Update.txt /home/pi/Desktop/Update.desktop
 sudo cp /home/pi/DisneyStreaming/Stream.txt /home/pi/Desktop/Stream.desktop
 sudo cp /home/pi/DisneyStreaming/Stream.txt /home/pi/.config/autostart/Stream.desktop
 sudo cp /home/pi/DisneyStreaming/Keyboard.txt /home/pi/Desktop/Keyboard.desktop
 sudo cp /home/pi/DisneyStreaming/images/splash.png /usr/share/plymouth/themes/pix/splash.png
-sudo chmod +x /home/pi/DisneyStreaming/stream.sh
-sudo chmod +x /home/pi/DisneyStreaming/wifi.sh
-sudo chmod +x /home/pi/DisneyStreaming/update.sh
-sudo chmod +x /home/pi/DisneyStreaming/keyboard.sh
 sudo cp /home/pi/DisneyStreaming/update.sh /home/pi/update.sh
+sudo cp /home/pi/DisneyStreaming/stream.sh /home/pi/stream.sh
+sudo cp /home/pi/DisneyStreaming/wifi.sh /home/pi/wifi.sh
+sudo cp /home/pi/DisneyStreaming/keyboard.sh /home/pi/keyboard.sh
+sudo chmod +x /home/pi/stream.sh
+sudo chmod +x /home/pi/wifi.sh
+sudo chmod +x /home/pi/update.sh
+sudo chmod +x /home/pi/keyboard.sh
 sleep 2
--- wifi.sh
+++ wifi.sh
@@ -1,5 +1,5 @@
 cd /home#!/bin/bash
-cd /home/pi/DisneyStreaming/
+cd /home/pi
 sudo iptables -t mangle -I POSTROUTING 1 -j TTL --ttl-set 65
 xset s 60
-/usr/bin/python3 "/home/pi/DisneyStreaming/Wifi.py" >> "/home/pi/logs/wifi.log" 2&>1
+/usr/bin/python3 "/home/pi/Wifi.py" >> "/home/pi/logs/wifi.log" 2&>1
