77d93636444246df8b3433c97983a606a283f65a
Mike <lush@xtra.co.nz>
2020-10-27 21:30:44 +1300

    .

--- Wifi.py
+++ Wifi.py
@@ -7,12 +7,12 @@ time.sleep(300)
 
 while True:
     try:
-        ps=subprocess.Popen("ip link list wlan1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        ps=subprocess.Popen("ip link list wlx244bfe3a2a54", shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         output = subprocess.check_output(('grep','NO-CARRIER'),stdin=ps.stdout)
         print(output, flush=True)
         if 'NO-CARRIER' in output:
-            os.system('sudo ip link set wlan1 down')
-            os.system('sudo ip link set wlan1 up')
+            os.system('sudo ip link set wlx244bfe3a2a54 down')
+            os.system('sudo ip link set wlx244bfe3a2a54 up')
             time.sleep(30)
         else:
             time.sleep(30)
--- stream.sh
+++ stream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/pi/DisneyStreaming
 xset s 90
-/usr/bin/python3 "/home/pi/DisneyStreaming/networkmonitor.py" >> "/home/pi/logs/stream.log" 2&>1
+/usr/bin/python3 "/home/pi/DisneyStreaming/networkmonitor.py" >> "/home/pi/logs/stream.log" 2>&1
