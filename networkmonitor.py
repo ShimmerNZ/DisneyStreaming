73701aa240ee0fffad2ac587c5a7a6e842d8a220
Mike <lush@xtra.co.nz>
2020-11-22 13:45:29 +1300

    Update networkmonitor.py

--- networkmonitor.py
+++ networkmonitor.py
@@ -383,7 +383,7 @@ class Mainframe(tk.Frame):
         self.txspeed=''
         if tx_prev > 0:
             tx_speed = ((tx - tx_prev)/100000)*5
-            print(time(),'TX: ',round(tx_speed,1), 'Mbps')
+            print(time,'TX: ',round(tx_speed,1), 'Mbps')
             self.txspeed=str(round(tx_speed,1))
         if rx_prev > 0:
             rx_speed = ((rx - rx_prev)/100000)*5
